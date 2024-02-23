8953cf41f82097e5eeaac091dd5af566444460c4
dilipsaini123 <138903046+dilipsaini123@users.noreply.github.com>
2024-02-23 11:24:18 +0530

    Update Program.cs

--- Optevus.WebAPI/Program.cs
+++ Optevus.WebAPI/Program.cs
@@ -26,11 +26,14 @@ builder.Services.AddScoped<IEmployeeService, EmployeesService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+  //  app.UseSwagger();
+  //  app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
